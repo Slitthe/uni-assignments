a217ab8eff113e0635c0c09a6bf6141a7129faed
Slitthe <sil.gherman@gmail.com>
2023-12-02 09:06:07 +0200

    Insertion sort

---
+++ year-2/semester-1/algorithms-and-data-structures/insertion_sort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+
+
+
+// Insertion sort
+int main() {
+    int input[] = {5, 2, 4, 6, 1, 3};
+    int size = sizeof(input) / sizeof(input[0]);
+
+    printf("\nBEFORE: \n");
+    for(int i = 0; i < size; i++) {
+        printf("input[%d]=%d\n", i, input[i]);
+    }
+
+    for(int i = 1; i < size; i++) {
+        for(int j = i; j > 0; j--) {
+            if(input[j - 1] > input[j]) {
+                int tmp = input[j - 1];
+                input[j - 1] = input[j];
+                input[j] = tmp;
+            }
+        }
+
+    }
+
+    printf("\nAFTER:\n");
+    for(int i = 0; i < size; i++) {
+        printf("input[%d]=%d\n", i, input[i]);
+    }
+
+    return 0;
+}
